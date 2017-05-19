b0e960760068f8d71b99b6b60b9199ffd3ee8808
eukaryote <minerguy31@gmail.com>
2017-05-19 14:20:00 -0600

    Make hash field longer

--- src/main/resources/game/sql/mkUserTable.sql
+++ src/main/resources/game/sql/mkUserTable.sql
@@ -1,7 +1,7 @@
 -- Makes the main users table
 CREATE TABLE `USERS` (
 	`username`	VARCHAR(100) NOT NULL UNIQUE,
-	`passhash`	VARCHAR(64) NOT NULL,
+	`passhash`	VARCHAR(100) NOT NULL,
 	`timeregistered`	BIGINT NOT NULL,
 	`balance`	BIGINT NOT NULL,
 	PRIMARY KEY(`username`)
